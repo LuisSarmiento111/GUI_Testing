b49931dc0ff8676e43fd4199c7398ac88bed606c
BT_4N2_30 <Lsarmiento9311@bths.edu>
2023-04-05 12:34:04 -0400

    GUI!!!!!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         JRPG JRPG = new JRPG();
+        System.out.println("hey");
     }
 }
